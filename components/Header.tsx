e1307bdd7fffaa7316727d9a5f8fa06c65238966
Dzikry Alfian <dzikryalfian1403@gmail.com>
2025-02-14 20:43:59 +0700

    feat: update header layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,11 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Navbar from "./Navbar";
-// import Image from "next/image";
-// import logo from "../public/assets/logo.svg";
-// import logoDark from "../public/assets/logoDark.svg";
+import Image from "next/image";
+import logo from "../public/assets/logo.svg";
+import logoDark from "../public/assets/logoDark.svg";
 import { MdDarkMode, MdLightMode } from "react-icons/md";
+import MobileNavbar from "./MNavbar";
 
 const Header = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -33,19 +34,19 @@ const Header = () => {
     <header className="py-8 xl:py-6 text-white">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        {/* <Link href={"/"}>
+        <Link href={"/"}>
           <Image
             src={darkMode ? logo : logoDark}
             className="w-28 md:w-[150px]"
             alt="logo"
           />
-        </Link> */}
+        </Link>
 
         {/* Dekstop Nav */}
         <div className="hidden lg:flex items-center gap-8">
         <Navbar />
           <Link href={"/contact"}>
-            <Button>Hire me</Button>
+            <Button>Contact Me</Button>
           </Link>
           {darkModeButton}
         </div>
@@ -53,6 +54,7 @@ const Header = () => {
         {/* Mobile Nav */}
         <div className="lg:hidden flex gap-2 md:gap-4">
           {darkModeButton}
+          <MobileNavbar />
         </div>
       </div>
     </header>
