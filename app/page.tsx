bb70ee61f729ef5ebba8fd30a99c393b4f9558f7
Dzikry Alfian <dzikryalfian1403@gmail.com>
2025-02-14 20:29:05 +0700

    edit : adjusting color and structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import CurrentTech from "@/components/CurrentTech";
+import Photo from "@/components/Photo";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,17 +14,20 @@ const Home = () => {
         <div className="flex flex-col lg:flex-row items-center justify-between lg:pt-8 lg:pb-12">
           {/* Aboute me */}
           <div className="text-center lg:text-left order-2 lg:order-none">
-            <h2 className="h2 text-cyan-600 mb-2">Dzikry Alfian</h2>
+            <h2 className="h2 text-sky-600 mb-2">Dzikry Alfian</h2>
             <span className="h3">Software Engineer</span>
             <p className="text-primary mt-4 dark:text-white leading-relaxed text-sm text-center lg:text-left lg:mr-14 text-md md:text-lg">
-            I am a web developer with one year of experience, specializing in frontend development using React.js and Vue.js. 
-            I have expertise in building responsive and interactive user interfaces while seamlessly integrating with backend services such as RESTful APIs and GraphQL
+              I am a web developer with one year of experience, 
+              specializing in frontend development using React.js and Vue.js. 
+              I have expertise in building responsive and interactive user interfaces 
+              while seamlessly integrating with backend services 
+              such as RESTful APIs and GraphQL
             </p>
 
             {/* Tech yang disukai */}
             <div className="text-sm md:text-md font-bold text-primary mt-4 dark:text-white">
               Tech experience :
-              
+              <CurrentTech />
             </div>
 
             {/* Button and social media */}
@@ -35,7 +40,7 @@ const Home = () => {
           </div>
           {/* Photo  */}
           <div className="order-1 lg:order-none mb-8 lg:mb-0 ">
-            
+            <Photo />
           </div>
         </div>
       </div>
