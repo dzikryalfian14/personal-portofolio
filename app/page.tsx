a3f37a30a6f504f6f587b470654bf7ce1a94f609
Dzikry Alfian <dzikryalfian1403@gmail.com>
2025-02-16 14:35:17 +0700

    edit : fix color default

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import CurrentTech from "@/components/CurrentTech";
 import Photo from "@/components/Photo";
+import Social from "@/components/Social";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -14,7 +15,7 @@ const Home = () => {
         <div className="flex flex-col lg:flex-row items-center justify-between lg:pt-8 lg:pb-12">
           {/* Aboute me */}
           <div className="text-center lg:text-left order-2 lg:order-none">
-            <h2 className="h2 text-sky-600 mb-2">Dzikry Alfian</h2>
+            <h2 className="h2 text-accent mb-2">Dzikry Alfian</h2>
             <span className="h3">Software Engineer</span>
             <p className="text-primary mt-4 dark:text-white leading-relaxed text-sm text-justify lg:text-left lg:mr-14 text-md md:text-lg">
               I am a web developer with one year of experience, 
@@ -36,7 +37,13 @@ const Home = () => {
             <div className="flex mt-8 flex-col lg:flex-row items-center gap-8">
               
               <div className="mb-8 lg:mb-0">
-                
+                <Social  
+                  containerStyles="flex gap-4"
+                  iconStyles="w-9 h-9 border border-accent 
+                  rounded-full flex justify-center items-center 
+                  text-accent text-base hover:bg-accent 
+                  hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
           </div>
