cae4166f9b4fc969d5e8588bd5686561b4a0bc28
Dzikry Alfian <dzikryalfian1403@gmail.com>
2025-02-23 15:39:58 +0700

    editing alert ui setting

--- app/portofolio/page.tsx
+++ app/portofolio/page.tsx
@@ -5,7 +5,7 @@ import PortofolioPage from "./portopage";
 export const metadata: Metadata = {
   title: "Dzikry Alfian | Work",
   description:
-    "Hi! my name is Bima Sanjaya - Explore the various projects and achievements I've worked on as a Software Engineer. This page showcases a range of my work, including software development projects, innovative solutions, and key contributions to the tech industry.",
+    "Hi! my name is Dzikry Alfian - Explore the various projects and achievements I've worked on as a Software Engineer. This page showcases a range of my work, including software development projects, innovative solutions, and key contributions to the tech industry.",
 };
 const Portofolio = () => {
   return <PortofolioPage />;
