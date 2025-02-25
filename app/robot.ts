02269f7b0ca0793aa1ef1fd8c9fc085fc9c98a76
Dzikry Alfian <dzikryalfian1403@gmail.com>
2025-02-25 20:41:28 +0700

    feat: add file configuration to control search engine crawling

---
+++ app/robot.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "Googlebot",
+        allow: ["/", "/about", "/services", "/portofolio", "/contact"],
+        disallow: "/private/",
+      },
+      {
+        userAgent: ["Applebot", "Bingbot"],
+        disallow: ["/"],
+      },
+    ],
+    sitemap: "https://dzikryalfian.vercel.app/sitemap.xml",
+  };
+}
